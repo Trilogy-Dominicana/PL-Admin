a75bc2b9abefe1b8ef9a477e9516898d0f1c7c18
Wilowayne de la Cruz <wdelacruz@viva.com.do>
2019-09-20 14:55:10 -0400

    Making .env functional and removing pycache dir.

--- src/database.py
+++ src/database.py
@@ -1,8 +1,13 @@
-import cx_Oracle
+import cx_Oracle, os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Database():
     def connect(self):
         '''Encharge to connect to Oracle database'''
-        connection = cx_Oracle.connect("ROAM_LOADER_APP", "VWWecatgGuAfQRnu", "192.168.100.242/DBTARIF.OM.DO")
+        print('Entering to database')
+        print(os.getenv("SERVICE_NAME"))
+        connection = 'Conectado'
+        # connection = cx_Oracle.connect("ROAM_LOADER_APP", "VWWecatgGuAfQRnu", "192.168.100.242/DBTARIF.OM.DO")
         return connection
  
