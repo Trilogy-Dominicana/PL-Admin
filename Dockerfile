ce3069e9d51cb6c3a7a0109444b70ea2aa1e4dd0
Wilowayne de la Cruz <wdelacruz@viva.com.do>
2019-10-18 15:03:24 -0400

    Adding package config compiler

--- Dockerfile
+++ Dockerfile
@@ -32,4 +32,7 @@ RUN python -m pip install --upgrade pip setuptools wheel && \
   python -m pip install tqdm && \
   python -m pip install --user --upgrade twine
 
+# Create package global
+# RUN 
+
 # CMD ["python --version"]
--- setup.py
+++ setup.py
@@ -3,16 +3,13 @@
 from setuptools import setup
 import os, sys, codecs
 
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
 
 setup_path = os.path.dirname(__file__)
 reqs_file = open(os.path.join(setup_path, 'requirements.txt'), 'r')
 reqs = reqs_file.readlines()
 reqs_file.close()
-
 
 setup(
     name="vivapl",
@@ -22,7 +19,7 @@ setup(
     author="Wilowayne De La Cruz",
     author_email="wdelacruz@viva.com.do",
     url="https://gitlab.viva.com.do/anaiboa/pl-pap",
-    packages=setup.find_packages(),
+    packages=["vivapl"],
     install_requires=reqs,
     # tests_require=tests_require,
     test_suite='nose.collector',
@@ -36,32 +33,3 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
-
-
-
-
-
-
-
-# setup(
-#     name='vivapl',  
-#     version='0.1',
-#     # scripts=['dokr'],
-#     author="Deepak Kumar",
-#     author_email="deepak.kumar.iet@gmail.com",
-#     description="A Docker and AWS utility package",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/javatechy/dokr",
-#     packages=setuptools.find_packages(),
-#     classifiers=[
-#         "Programming Language :: Python :: 3",
-#         "License :: OSI Approved :: MIT License",
-#         "Operating System :: OS Independent",
-#     ],
-#  )
-
-
-# from setuptools import setup
-# tests_require = ['nose', 'mock', 'responses==0.5.1', 'unittest2']
-
